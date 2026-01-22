93a41180d9ca7ea9db81666dbb4bcdfb64dc9c37
ivaldas <ivaldas@gmail.com>
2026-01-22 13:42:15 -0500

    updated insert_filmsdata_sql and select_filmsdata_sql

--- controllers/sql/insert_filmsdata_sql.js
+++ controllers/sql/insert_filmsdata_sql.js
@@ -4,17 +4,16 @@ import films_data from "../../models/films_data.json" with { type: "json" };
 
 export const addFilms = async (req, res) => {
   try {
-    films_data.forEach((film) => {
-      insertFilm(film);
-    });
-    // insertFilm(films_data)
-    res.status(200).send("films added successfully");
+    for (const film of films_data) {
+      await insertFilm(film);
+    }
+    res.status(200).send("Films added successfully");
   } catch (err) {
-    res.status(404).send(err.message);
+    res.status(404).send(`Error: ${err.message}`);
   }
 };
 
-function insertFilm(film) {
+const insertFilm = async (film) => {
   const {
     file_path,
     full_path,
@@ -36,132 +35,117 @@ function insertFilm(film) {
 
   const checkFilmQuery =
     "SELECT COUNT(*) AS count FROM films WHERE programid = ?";
-  db.query(checkFilmQuery, [film.programid], (err, result) => {
-    if (err) {
-      console.error("Error checking film existence:", err);
-      return;
-    }
+  const [checkFilmResult] = await db.query(checkFilmQuery, [film.programid]);
 
-    if (result[0].count === 0) {
-      const insertFilmQuery =
-        "INSERT INTO films (file_path, full_path, movieyear, title, description, isepisode, isepisodic, seriesid, programid, starrating, mpaarating, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-      db.query(
-        insertFilmQuery,
-        [
-          file_path,
-          full_path,
-          movieyear,
-          title,
-          description,
-          isepisode,
-          isepisodic,
-          seriesid,
-          programid,
-          starrating,
-          mpaarating,
-          image,
-        ],
-        (err, result) => {
-          if (err) {
-            console.error("Error inserting movie:", err);
-            return;
-          }
+  if (checkFilmResult[0].count === 0) {
+    const insertFilmQuery = `
+      INSERT INTO films (file_path, full_path, movieyear, title, description, 
+      isepisode, isepisodic, seriesid, programid, starrating, mpaarating, image) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
 
-          const film_id = result.insertId; // Get the inserted film_id
-          insertFilmCrew(
-            directors,
-            "directors",
-            "filmdirectors",
-            "director_id",
-            film_id,
-          );
-          insertFilmCrew(
-            writers,
-            "writers",
-            "filmwriters",
-            "writer_id",
-            film_id,
-          );
-          insertFilmCrew(actors, "actors", "filmactors", "actor_id", film_id);
-          insertGenres(programgenre, film_id);
-        },
+    const [filmInsertResult] = await db.query(insertFilmQuery, [
+      file_path,
+      full_path,
+      movieyear,
+      title,
+      description,
+      isepisode,
+      isepisodic,
+      seriesid,
+      programid,
+      starrating,
+      mpaarating,
+      image,
+    ]);
+
+    const film_id = filmInsertResult.insertId;
+
+    await insertFilmCrew(
+      directors,
+      "directors",
+      "filmdirectors",
+      "director_id",
+      film_id,
+    );
+    await insertFilmCrew(
+      writers,
+      "writers",
+      "filmwriters",
+      "writer_id",
+      film_id,
+    );
+    await insertFilmCrew(actors, "actors", "filmactors", "actor_id", film_id);
+    await insertGenres(programgenre, film_id);
+  } else {
+    console.log("Film already exists.");
+  }
+};
+
+const insertFilmCrew = async (
+  table,
+  tableName,
+  relationTable,
+  person_id,
+  film_id,
+) => {
+  for (const person of table) {
+    let personName = person.replace("|", " ").toString();
+
+    const [result] = await db.query(
+      `SELECT ${person_id} FROM ${tableName} WHERE name = ?`,
+      [personName],
+    );
+
+    if (result.length > 0) {
+      const personId = result[0][`${person_id}`];
+      console.log(`'${personName}' already exists with ID: ${personId}`);
+
+      // Insert into the relation table (Many-to-Many Relationship)
+      await db.query(
+        `INSERT INTO ${relationTable} (film_id, ${person_id}) VALUES (?, ?)`,
+        [film_id, personId],
       );
     } else {
-      console.log("Film already exists.");
+      // If person doesn't exist, insert new person
+      const [insertResult] = await db.query(
+        `INSERT INTO ${tableName} (name) VALUES (?)`,
+        [personName],
+      );
+      const personId = insertResult.insertId;
+
+      // Link film and person in relation table
+      await db.query(
+        `INSERT INTO ${relationTable} (film_id, ${person_id}) VALUES (?, ?)`,
+        [film_id, personId],
+      );
     }
-  });
-}
+  }
+};
 
-function insertFilmCrew(table, tableName, relationTable, person_id, film_id) {
-  table.forEach((person) => {
-    let personsName = person.replace("|", " ").toString();
-    db.query(
-      `SELECT ${person_id} FROM ${tableName} WHERE name = ?`,
-      [personsName],
-      (err, result) => {
-        if (err) throw err;
-
-        let personId;
-        if (result.length > 0) {
-          // person exists, use existing ID
-          personId = result[0][`${person_id}`];
-          console.log(`'${personsName}' already exists with ID: ${personId}`);
-          db.query(
-            `INSERT INTO ${relationTable} (film_id, ${person_id}) VALUES (?, ?)`,
-            [film_id, personId],
-            (err) => {
-              if (err) console.error(`Error linking film with ${person}`, err);
-            },
-          );
-        } else {
-          // person does not exist, insert new person
-          db.query(
-            `INSERT INTO ${tableName} (name) VALUES (?)`,
-            // "INSERT IGNORE INTO actors (name) VALUES (?) ON DUPLICATE KEY UPDATE actor_id = actor_id"
-            [personsName],
-            (err, result) => {
-              if (err) {
-                console.error(`Error inserting ${person}`, err);
-                return;
-              }
-              const id = result.insertId;
-              // Insert into film_person Table (Many-to-Many Relationship)
-              db.query(
-                `INSERT INTO ${relationTable} (film_id, ${person_id}) VALUES (?, ?)`,
-                // 'UPDATE movies SET director_id = ? WHERE movie_id = ?',
-                [film_id, id],
-                (err) => {
-                  if (err)
-                    console.error(`Error linking film with ${person}:`, err);
-                },
-              );
-            },
-          );
-        }
-      },
-    );
-  });
-}
-
-function insertGenres(programgenre, film_id) {
-  programgenre.forEach((genre) => {
-    db.query(
-      "INSERT INTO genres (genre_name) VALUES (?)",
+const insertGenres = async (programgenre, film_id) => {
+  for (const genre of programgenre) {
+    const [genreResult] = await db.query(
+      "SELECT genre_id FROM genres WHERE genre_name = ?",
       [genre],
-      (err, result) => {
-        if (err) {
-          console.error("Error inserting genre:", err);
-          return;
-        }
-        const genre_id = result.insertId;
-        db.query(
-          "INSERT INTO filmgenres (film_id, genre_id) VALUES (?, ?)",
-          [film_id, genre_id],
-          (err) => {
-            if (err) console.error("Error linking film with genre:", err);
-          },
-        );
-      },
     );
-  });
-}
+
+    let genre_id;
+    if (genreResult.length > 0) {
+      genre_id = genreResult[0].genre_id;
+    } else {
+      // Insert genre if it doesn't exist
+      const [insertGenreResult] = await db.query(
+        "INSERT INTO genres (genre_name) VALUES (?)",
+        [genre],
+      );
+      genre_id = insertGenreResult.insertId;
+    }
+
+    // Link genre to film
+    await db.query("INSERT INTO filmgenres (film_id, genre_id) VALUES (?, ?)", [
+      film_id,
+      genre_id,
+    ]);
+  }
+};
